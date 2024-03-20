44fc58a84afece7a9de0e297a63b30c50048ff1b
nliu12333 <156932719+nliu12333@users.noreply.github.com>
2024-03-20 20:54:21 +0000

    Add files via upload

---
+++ 3_ReadSensorHighcharts/3_ReadSensorHighcharts.ino
@@ -0,0 +1,254 @@
+#include <Adafruit_ADXL345_U.h>
+
+// Import required libraries
+// #ifdef ESP32
+//   #include <WiFi.h>
+//   #include <ESPAsyncWebServer.h>
+//   #include <SPIFFS.h>
+// #else
+  #include <Arduino.h>
+  #include <ESP8266WiFi.h>
+  #include <Hash.h>
+  #include <ESPAsyncTCP.h>
+  #include <ESPAsyncWebServer.h>
+  #include <FS.h>
+// #endif
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void displayDataRate(void)
+{
+  Serial.print  ("Data Rate:    "); 
+  
+  switch(accel.getDataRate())
+  {
+    case ADXL345_DATARATE_3200_HZ:
+      Serial.print  ("3200 "); 
+      break;
+    case ADXL345_DATARATE_1600_HZ:
+      Serial.print  ("1600 "); 
+      break;
+    case ADXL345_DATARATE_800_HZ:
+      Serial.print  ("800 "); 
+      break;
+    case ADXL345_DATARATE_400_HZ:
+      Serial.print  ("400 "); 
+      break;
+    case ADXL345_DATARATE_200_HZ:
+      Serial.print  ("200 "); 
+      break;
+    case ADXL345_DATARATE_100_HZ:
+      Serial.print  ("100 "); 
+      break;
+    case ADXL345_DATARATE_50_HZ:
+      Serial.print  ("50 "); 
+      break;
+    case ADXL345_DATARATE_25_HZ:
+      Serial.print  ("25 "); 
+      break;
+    case ADXL345_DATARATE_12_5_HZ:
+      Serial.print  ("12.5 "); 
+      break;
+    case ADXL345_DATARATE_6_25HZ:
+      Serial.print  ("6.25 "); 
+      break;
+    case ADXL345_DATARATE_3_13_HZ:
+      Serial.print  ("3.13 "); 
+      break;
+    case ADXL345_DATARATE_1_56_HZ:
+      Serial.print  ("1.56 "); 
+      break;
+    case ADXL345_DATARATE_0_78_HZ:
+      Serial.print  ("0.78 "); 
+      break;
+    case ADXL345_DATARATE_0_39_HZ:
+      Serial.print  ("0.39 "); 
+      break;
+    case ADXL345_DATARATE_0_20_HZ:
+      Serial.print  ("0.20 "); 
+      break;
+    case ADXL345_DATARATE_0_10_HZ:
+      Serial.print  ("0.10 "); 
+      break;
+    default:
+      Serial.print  ("???? "); 
+      break;
+  }  
+  Serial.println(" Hz");  
+}
+
+void displayRange(void)
+{
+  Serial.print  ("Range:         +/- "); 
+  
+  switch(accel.getRange())
+  {
+    case ADXL345_RANGE_16_G:
+      Serial.print  ("16 "); 
+      break;
+    case ADXL345_RANGE_8_G:
+      Serial.print  ("8 "); 
+      break;
+    case ADXL345_RANGE_4_G:
+      Serial.print  ("4 "); 
+      break;
+    case ADXL345_RANGE_2_G:
+      Serial.print  ("2 "); 
+      break;
+    default:
+      Serial.print  ("?? "); 
+      break;
+  }  
+  Serial.println(" g");  
+}
+
+
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
+
+//Adafruit_ADXL345_U adxl; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+// Replace with your network credentials
+const char* ssid = "NingweisiPhone";
+const char* password = "1A2B3C4D";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+
+/* Get a new sensor event */ 
+sensors_event_t event;
+
+String readXAcceleration() {
+  // Read temperature as Celsius (the default)
+  float t = event.acceleration.x;
+  // Convert temperature to Fahrenheit
+  //t = 1.8 * t + 32;
+  if (isnan(t)) {
+    Serial.println("Failed to read from sensor!");
+    return "";
+  }
+    else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readYAcceleration() {
+  float h = event.acceleration.y;
+  if (isnan(h)) {
+    Serial.println("Failed to read from sensor!");
+    return "";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+String readZAcceleration() {
+  float p = event.acceleration.z;
+  if (isnan(p)) {
+    Serial.println("Failed to read from sensor!");
+    return "";
+  }
+  else {
+    Serial.println(p);
+    return String(p);
+  }
+}
+
+void setup(){
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+  Serial.begin(115200);
+  Serial.println("Accelerometer Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+  // accel.setRange(ADXL345_RANGE_8_G);
+  // accel.setRange(ADXL345_RANGE_4_G);
+  // accel.setRange(ADXL345_RANGE_2_G);
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Display additional settings (outside the scope of sensor_t) */
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  accel.getEvent(&event);
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  server.on("/XAcceleration", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readXAcceleration().c_str());
+  });
+  server.on("/YAcceleration", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readYAcceleration().c_str());
+  });
+  server.on("/ZAcceleration", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readZAcceleration().c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
